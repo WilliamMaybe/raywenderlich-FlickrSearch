8bec745f082ff47b82aef9afb7683f837f62ca2d
WilliamMaybe <271138178@qq.com>
2016-03-08 21:55:54 +0800

    Initializer

---
+++ FlickrSearch/FlickrSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrSearch
+//
+//  Created by WilliamZhang on 16/3/8.
+//  Copyright © 2016年 WilliamZhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # raywenderlich-FlickrSearch
-raywenderlich FlickrSearch
+
+好久没学了，当初看到卡片翻书效果，一直想弄，却停在了Swift的恐惧之下，现在在回来看，熟悉练习下CollectionView。
+
+[UICollectionView Tutorial Part 1: Getting Started](https://www.raywenderlich.com/78550/beginning-ios-collection-views-swift-part-1)
